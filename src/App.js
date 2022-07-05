5b7a8d85d1af69affe5512cb5e4e8f60a09055dc
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-07-05 13:04:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render(){
+    return (
+      <div className="App">
+      <CardList name='shreyas'>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}> { monster.name } </h1>
+          ))
+        }
+      </CardList>
+    </div>
+    )
+  }
+}
+
+export default App;
