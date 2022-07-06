29f5a501798fc7d346c3e1f70b9f5e78cc7e57ed
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-07-06 20:00:53 +0530

    secondary commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -18,17 +20,21 @@ class App extends Component {
   }
 
   render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonster = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
-      <div className="App">
-      <CardList name='shreyas'>
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={monster.id}> { monster.name } </h1>
-          ))
-        }
-      </CardList>
+    <div className="App">
+      <h1> Monster Rolodex </h1>
+      <SearchBox
+        placeholder= 'Search Monster'
+        handleChange={(e)=> this.setState({ searchField: e.target.value })}
+      />
+      <CardList monsters = { filteredMonster }/>
     </div>
-    )
+    );
   }
 }
 
